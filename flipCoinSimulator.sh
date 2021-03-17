c00452dbdea2f34740d649c4bb78b1f12c72f8b7
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-18 01:37:45 +0530

    Add: If tied, continued untill one won by 2 points

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,6 +4,7 @@ Heads=1
 Tails=0
 headCount=0
 tailCount=0
+
 while [ true ]
 do
 	result=$((RANDOM%2))
@@ -25,7 +26,16 @@ done
 
 if [ $headCount -eq $tailCount ]
 then
-	echo "Its a tie!"
+	while [ $(($headCount-$tailCount)) -eq 2 ]
+	do
+		result=$((RANDOM%2))
+		if [ $result -eq $Heads ]
+		then
+			((headCount++))
+		else
+			((tailCount++))
+		fi
+	done
 elif [ $headCount -gt $tailCount ]
 then
 	echo "Heads won by $(($headCount-$tailCount)) majority"
